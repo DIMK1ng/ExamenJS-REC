343aed054ea81187d7a7acb3bee01cb85f8adb68
DIMK1ng <davidmezzi10@gmail.com>
2022-03-02 19:39:24 +0100

    Examen

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-function _id(ident){
+function _id(ident) {
     return document.getElementById(ident);
 }
 
-function calculaHora(){
-    
+function calculaHora() {
+    let ponerTiempo = (_id("inputTime").value);
+    let separarTiempo = ponerTiempo.split(":");
+    let tipusTime = (_id("tipusTime").value);
+    let resultatTime;
+
+    let hora = parseInt(separarTiempo[0]);
+    let minutos = parseInt(separarTiempo[1]);
+
+    if (tipusTime == "m") {
+        resultatTime = (hora * 60) + minutos;
+        resultatTime += " Minutos"
+    } else if (tipusTime == "s") {
+        resultatTime = (hora * 60 + minutos) * 60;
+        resultatTime += " Segundos"
+    }
+
+    _id("resultatTime").innerHTML = resultatTime;
+
 }
 
-function calculaCapital(){
-    
+function calculaCapital() {
+
 }
 
-function canviaEstil(tipus){
-    
+function canviaEstil(tipus) {
+    var colorFons;
+    var colorBorder;
+    if (tipus == "clear") {
+        colorFons = "white";
+        colorBorder = "black";
+    } else if (tipus == "random") {
+        var red = Math.floor(Math.random() * 255);
+        var green = Math.floor(Math.random() * 255);
+        var blue = Math.floor(Math.random() * 255);
+        colorFons = "rgb(" + red + "," + green + "," + blue + ")";
+        colorBorder = "rgb(" + blue + "," + red + "," + green + ")";
+    }
+
+
+    _id("principal").style.backgroundColor = colorFons;
+    _id("principal").style.borderColor = colorBorder;
 }
